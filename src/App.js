478d8b82041c41be24fd36a3d0f1bb97d315cc90
Dodger-42 <kevin.bing.barber@gmail.com>
2020-07-17 16:49:22 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Switch } from 'react-router-dom';
+import styled from 'styled-components'; 
+import HomePage from "./HomePage";
 
 const App = () => {
   return (
+    <AppContainer>
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
     </>
+    <div>
+      <Pizza />
+      <HomePage orders={orders} />
+    </div>
+    </AppContainer>
   );
 };
+
+const AppContainer = styled.div`
+
+`
 export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = (props) => {
+  return (
+    <>
+      <h1>Lambda Eats</h1>
+      <p>You can remove this code and create your own header</p>
+    </>
+  );
+};
+export default HomePage;
