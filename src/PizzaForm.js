8bd83c3edffb426266cfb9df075b293360088811
Dodger-42 <kevin.bing.barber@gmail.com>
2020-07-17 18:35:28 -0700

    first

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -77,42 +77,79 @@ const PizzaForm = (props) => {
   return (
     <FormContainer>
       <form onSubmit={handleSubmit}>
-            <label>Name
-                <input type='text' name='name' onChange={handleChange} data-cy='name' value={formState.name} />
-                {errors.name.length > 0 && <p style={{color:'red'}}>{errors.name}</p>}
-      </label>
-      <label>
-          <input type='checkbox' name='toppings' onChange={handleChange} data-cy='sausage' value='sausage' />
-          Sausage
-      </label>
-      <label>
-          <input type='checkbox' name='toppings' onChange={handleChange} data-cy='ham' value='ham' />
-          Ham
-      </label>
-      <label>
-          <input type='checkbox' name='toppings' onChange={handleChange} data-cy='mushrooms' value='mushrooms' />
-          Mushrooms
-      </label>
-      <label>
-          <input type='checkbox' name='toppings' onChange={handleChange} data-cy='olives' value='olives' />
-          Olives
-      </label>
-      <label>
-          <input type='checkbox' name='toppings' onChange={handleChange} data-cy='cheese' value='cheese' />
-          Cheese
-      </label>
-      <label>
-          <input type='checkbox' name='toppings' onChange={handleChange} data-cy='onion' value='onion' />
-          Onion
-      </label>
-      <label>
-          <input type='checkbox' name='toppings' onChange={handleChange} data-cy='jalapeno' value='jalapeno' />
-          Jalapeno
-      </label>
-      <label>
-          <input type='checkbox' name='toppings' onChange={handleChange} data-cy='bacon' value='bacon' />
-          Bacon
-      </label>
+          <label>Name
+                    <input type='text' name='name' onChange={handleChange} data-cy='name' value={formState.name} />
+                    {errors.name.length > 0 && <p style={{color:'red'}}>{errors.name}</p>}
+          </label>
+          <label>Phone Number
+                    <input type='tel' name='phone' onChange={handleChange} data-cy='phone' value={formState.phone} />
+                    {errors.phone.length > 0 && <p style={{color:'red'}}>{errors.phone}</p>}
+          </label>
+          <label>Select Pizza Size
+                <select name='size' data-cy='size' defaultValue='Medium' onChange={handleChange}>
+                    <option value='Small'>Small</option>
+                    <option value='Medium'>Medium</option>
+                    <option value='Large'>Large</option>
+                    <option value='Extra Large'>Extra Large</option>
+                    <option value='Personal'>Personal</option>
+                </select>
+            </label>
+      {/* <fieldset>
+          <label>
+              <input checked type='size' name='size' onChange={handleChange} data-cy='small' value='small' />
+              Small Pizza
+          </label>
+          <label>
+              <input type='size' name='size' onChange={handleChange} data-cy='medium' value='medium' />
+              Medium Pizza
+          </label>
+          <label>
+              <input type='size' name='size' onChange={handleChange} data-cy='large' value='large' />
+              Large Pizza
+          </label>
+          <label>
+              <input type='size' name='size' onChange={handleChange} data-cy='ex-large' value='ex-large' />
+              Extra Large Pizza
+          </label>
+      </fieldset> */}
+      <fieldset>
+          <label>
+              <input type='checkbox' name='toppings' onChange={handleChange} data-cy='sausage' value='sausage' />
+              Sausage
+          </label>
+          <label>
+              <input type='checkbox' name='toppings' onChange={handleChange} data-cy='ham' value='ham' />
+              Ham
+          </label>
+          <label>
+              <input type='checkbox' name='toppings' onChange={handleChange} data-cy='mushrooms' value='mushrooms' />
+              Mushrooms
+          </label>
+          <label>
+              <input type='checkbox' name='toppings' onChange={handleChange} data-cy='olives' value='olives' />
+              Olives
+          </label>
+          <label>
+              <input type='checkbox' name='toppings' onChange={handleChange} data-cy='cheese' value='cheese' />
+              Cheese
+          </label>
+          <label>
+              <input type='checkbox' name='toppings' onChange={handleChange} data-cy='onion' value='onion' />
+              Onion
+          </label>
+          <label>
+              <input type='checkbox' name='toppings' onChange={handleChange} data-cy='jalapeno' value='jalapeno' />
+              Jalapeno
+          </label>
+          <label>
+              <input type='checkbox' name='toppings' onChange={handleChange} data-cy='bacon' value='bacon' />
+              Bacon
+          </label>
+      </fieldset>
+          <label>
+                <textarea name='instructions' data-cy='instructions' onChange={handleChange} value={formState.instructions} />
+          </label>
+            <button data-cy="submit-button" disabled={isDisabled} type='submit'>Order Your Pizza</button>
       </form>
     </FormContainer>
     
