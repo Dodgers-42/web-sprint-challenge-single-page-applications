73faee0466ccb9706361d61d14d79ae6f1c2c974
Dodger-42 <kevin.bing.barber@gmail.com>
2020-07-17 17:45:34 -0700

    first

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -5,10 +5,45 @@ import axios from 'axios';
 
 const schema = yup.object().shape({
   name: yup.string().required('Please enter your name').min(2, 'That\'s just your inicials.'),
-  // phone: yup.string().required('Please enter a phone number.').matches(/^[0-9]{10}$/, 'Please enter a valid phone number.')
+  phone: yup.string().required('Please enter a phone number.').matches(/^[0-9]{10}$/, 'Please enter a valid phone number.')
 })
 
+const defaultFormState = {
+  name: '',
+  phone: '',
+  toppings: {
+      sausage: false,
+      ham: false,
+      mushrooms: false,
+      olives: false,
+      cheese: false,
+      onion: false,
+      jalapeno: false,
+      bacon: false
+  },
+  instructions: ''
+}
+
+const defaultErrorState = {
+  name: '',
+  phone: ''
+}
+
 const PizzaForm = (props) => {
+    const [formState, setFormState] = useState(defaultFormState);
+    const [errors, setErrors] = useState(defaultErrorState);
+    const [isDisabled, setIsDisabled] = useState(true);
+
+    useEffect(() => {
+        schema.isValid(formState).then(valid => setIsDisabled(!valid));
+    }, [formState, schema])
+
+    const validate = e => {
+        e.persist();
+        yup.reach(schema, e.target.name).validate(e.target.value)
+        .then(valid => setErrors({...errors, [e.target.name]: ''}))
+        .catch(err => setErrors({...errors, [e.target.name]: err.errors[0]}));
+    }
   return (
     <FormContainer>
       <form>
