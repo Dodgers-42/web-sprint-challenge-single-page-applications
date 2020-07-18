7b0227a049b20eb885e71c236e7d6281a8dd3a03
Dodger-42 <kevin.bing.barber@gmail.com>
2020-07-17 17:36:29 -0700

    first

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -3,13 +3,30 @@ import * as yup from 'yup';
 import styled from 'styled-components';
 import axios from 'axios';
 
+const schema = yup.object().shape({
+  name: yup.string().required('Please enter your name').min(2, 'That\'s just your inicials.'),
+  // phone: yup.string().required('Please enter a phone number.').matches(/^[0-9]{10}$/, 'Please enter a valid phone number.')
+})
 
 const PizzaForm = (props) => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <FormContainer>
+      <form>
+      <label>
+        <input/>
+      </label>
+      <label>
+        <input/>
+      </label>
+      <label>
+        <input/>
+      </label>
+      <label>
+        <input/>
+      </label>
+      </form>
+    </FormContainer>
+    
   );
 };
 export default PizzaForm;
